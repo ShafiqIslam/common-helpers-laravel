383f484c684f71050b1d628976aa80c98271dd53
Shafiqul Islam <shafiqul@sheba.xyz>
2022-12-11 11:26:46 +0600

    ➕ Add helper function for enum display value
    Type(s): ADD

--- src/Enum/GetWithDisplayValue.php
+++ src/Enum/GetWithDisplayValue.php
@@ -8,8 +8,17 @@ trait GetWithDisplayValue
 {
     public static function withDisplayValues(): array
     {
+        return self::getDisplayValues(self::cases());
+    }
+
+    /**
+     * @param array<HasDisplayValue> $cases
+     * @return array
+     */
+    public static function getDisplayValues(array $cases): array
+    {
         return Arr::mapAssoc(function ($key, HasDisplayValue $type) {
             return [$type->value, $type->getDisplayValue()];
-        }, self::cases());
+        }, $cases);
     }
 }
