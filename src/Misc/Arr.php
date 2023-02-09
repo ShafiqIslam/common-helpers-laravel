8ae28153e6c136ac7cdb145f8b62cbb654597ae4
Shafiqul Islam <shafiqul@sheba.xyz>
2023-02-09 14:02:40 +0600

    ➕ Add array helper function
    Type(s): ADD

--- src/Misc/Arr.php
+++ src/Misc/Arr.php
@@ -13,4 +13,16 @@ class Arr
     {
         return array_column(array_map($f, array_keys($a), $a), 1, 0);
     }
+
+    /**
+     * @param callable $f
+     * @param array $a
+     * @return array
+     */
+    public static function mapAssocAndPreserveKeys(callable $f, array $a)
+    {
+        return self::mapAssoc(function (string $key, $value) use ($f) {
+            return [$key, $f($value)];
+        }, $a);
+    }
 }
