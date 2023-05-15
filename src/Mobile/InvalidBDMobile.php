8b171a39f63c8af06ff897864d90bcf4eed8c6c6
Shafiqul Islam <shafiqul@sheba.xyz>
2023-05-15 21:04:50 +0600

    ➕ Add generic renderer in invalid mobile
    Type(s): ADD

--- src/Mobile/InvalidBDMobile.php
+++ src/Mobile/InvalidBDMobile.php
@@ -4,9 +4,12 @@ namespace Polygontech\CommonHelpers\Mobile;
 
 use Exception;
 use Throwable;
+use Polygontech\CommonHelpers\Misc\GenericExceptionRenderer;
 
 class InvalidBDMobile extends Exception
 {
+    use GenericExceptionRenderer;
+
     public function __construct(string $message = "This is not a Bangladeshi Mobile Number", int $code = 400, ?Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
